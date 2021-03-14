693b7895636b11218144d795e9b1d91e3ea2e804
Mike Partridge <halcyon22@gmail.com>
2021-03-14 11:57:54 -0500

    working using navigation

--- app/src/main/java/com/example/wordsapp/WordAdapter.kt
+++ app/src/main/java/com/example/wordsapp/WordAdapter.kt
@@ -27,72 +27,48 @@ import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 
-/**
- * Adapter for the [RecyclerView] in [DetailActivity].
- */
-class WordAdapter(private val letterId: String, context: Context) :
-    RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
+class WordAdapter(private val selectedLetter: String, context: Context) : RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
 
     private val filteredWords: List<String>
 
     init {
-        // Retrieve the list of words from res/values/arrays.xml
-        val words = context.resources.getStringArray(R.array.words).toList()
-
-        filteredWords = words
-            // Returns items in a collection if the conditional clause is true,
-            // in this case if an item starts with the given letter,
-            // ignoring UPPERCASE or lowercase.
-            .filter { it.startsWith(letterId, ignoreCase = true) }
-            // Returns a collection that it has shuffled in place
-            .shuffled()
-            // Returns the first n items as a [List]
-            .take(5)
-            // Returns a sorted version of that [List]
+        val allWords = context.resources.getStringArray(R.array.words).toList()
+        filteredWords = allWords
+            .filter { it.startsWith(selectedLetter, ignoreCase = true) }
             .sorted()
     }
 
     class WordViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val button = view.findViewById<Button>(R.id.button_item)
+        val wordButton: Button = view.findViewById(R.id.button_item)
     }
 
     override fun getItemCount(): Int = filteredWords.size
 
-    /**
-     * Creates new views with R.layout.item_view as its template
-     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
         val layout = LayoutInflater
             .from(parent.context)
             .inflate(R.layout.item_view, parent, false)
 
-        // Setup custom accessibility delegate to set the text read
         layout.accessibilityDelegate = Accessibility
 
         return WordViewHolder(layout)
     }
 
-    /**
-     * Replaces the content of an existing view with new data
-     */
     override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
-
-        val item = filteredWords[position]
-        // Needed to call startActivity
+        val word = filteredWords[position]
         val context = holder.view.context
 
-        // Set the text of the WordViewHolder
-        holder.button.text = item
-        holder.button.setOnClickListener {
-            val queryUrl: Uri = Uri.parse("${DetailActivity.SEARCH_PREFIX}${item}")
+        holder.wordButton.text = word
+        holder.wordButton.setOnClickListener {
+            val queryUrl: Uri = Uri.parse("${WordListFragment.SEARCH_PREFIX}${word}")
             val intent = Intent(Intent.ACTION_VIEW, queryUrl)
             context.startActivity(intent)
         }
-
     }
 
-    // Setup custom accessibility delegate to set the text read with
-    // an accessibility service
+    /**
+     * Setup custom accessibility delegate to set the text read with an accessibility service
+     */
     companion object Accessibility : View.AccessibilityDelegate() {
         @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
         override fun onInitializeAccessibilityNodeInfo(
